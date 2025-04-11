c20483f736def016aa5b48d2c6cc1c7025378848
ali <Alinazim363@gmail.com>
2025-04-10 20:06:53 -0400

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,7 +1,7 @@
 gcloud container clusters create todolist-cluster \
---project=cisc-5550 \
---zone=us-central1-a
+ --project=cisc-5550 \
+ --zone=us-central1-a
 
 gcloud container clusters get-credentials todolist-cluster \
---project=cisc-5550 \
---zone=us-central1-a
+ --project=cisc-5550 \
+ --zone=us-central1-a
