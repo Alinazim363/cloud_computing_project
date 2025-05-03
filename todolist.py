684a5136358170ac02c9138b3dda9bf2f3c7e13a
ali <Alinazim363@gmail.com>
2025-05-03 16:03:57 -0400

    updated ip address

--- todolist.py
+++ todolist.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 app = Flask(__name__)
-TODO_API_URL = os.getenv("TODO_API_URL", "http://34.135.80.40:5001")
+TODO_API_URL = os.getenv("TODO_API_URL", "http://34.55.28.240:5001")
 
 
 
